01358af78a66ec14903fa2560c2bbd3480bd9037
DanijelMi <danijel.milosevic.it@gmail.com>
2019-03-09 10:27:01 +0000

    disable right click context menu

--- awesome/keybinds.lua
+++ awesome/keybinds.lua
@@ -1,6 +1,6 @@
 -- {{{ Mouse bindings
 root.buttons(my_table.join(
-    awful.button({ }, 3, function () awful.util.mymainmenu:toggle() end),
+--    awful.button({ }, 3, function () awful.util.mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
