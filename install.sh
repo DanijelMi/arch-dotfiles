c526b4314ca629b5499169e4e211d231bffc3ac3
DanijelMi <danijel.milosevic.it@gmail.com>
2019-03-17 18:58:59 +0100

    add default options

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # Symlinks all the config files from the central repository
+
+# If this script is not located at the base directory of the dotfile repository, it will not work.
+# Gets the current directory location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 declare -a dotpaths
 
+#####    DEFINE YOUR SYMLINKS HERE #############
+# FORMAT: "PATH_TO_REPO_CONFIG:PATH_IN_SYSTEM"
 dotpaths=(
-"awesomea:~/.config/awesome" 
+"awesome:~/.config/awesome1" 
 "neovim:~/.config/nvim" 
 ".bashrc:~/.bashrc" 
 )
+################################################
 
 for i in "${dotpaths[@]}"
 do
@@ -24,12 +30,23 @@ do
 		# Check if target already exists
 		elif [ -e ${paths_split[1]} ]; 
 		then
-		    echo "Warning: ${paths_split[1]} already exists"
+		    while true; do
+			    read -p "${paths_split[1]} already exists!
+Do you want to overwrite/skip/overwrite with backup (y/n/B)? " yn
+			    if [ ${#yn} == 0 ] ; then yn="B" ; fi
+			    case $yn in
+				    [Yy]* ) echo Overwrtie; break;;
+				    [Nn]* ) echo Skip; break;;
+				    [Bb]* ) echo Backup; break;;
+				    * ) echo Invalid answer;;
+		    esac
+		    done
 		    break;
 		fi
 		read -p "Symlink \"$DIR/${paths_split[0]} into ${paths_split[1]}\"(Y/n)? " yn
+	    	if [ ${#yn} == 0 ] ; then yn="Y" ; fi
 		case $yn in
-			[Yy]* ) echo "Yes!"; break;;
+			[Yy]* ) echo ln -srf $PWD/${paths_split[0]} ${paths_split[1]}; break;;
 			[Nn]* ) echo "No!"; break ;;
 			* ) echo "Invalid answer";;
 		esac
