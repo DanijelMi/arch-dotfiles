abdb4a6770a9f2729ed16bdc76d476e58413f03c
DanijelMi <danijel.milosevic.it@gmail.com>
2019-05-13 11:32:38 +0200

    explode arguments, dependency checking

--- scripts/glitch-lock.sh
+++ scripts/glitch-lock.sh
@@ -1,9 +1,41 @@
 #!/usr/bin/env bash
-# Takes a picture of desktop with scrot, applies glitchy effects to it
-# Then locks screen and applying the glitchy pic as background
-# https://github.com/r00tman/corrupter
-tmpbg="/tmp/screen.png"
-scrot --silent "$tmpbg"
-corrupter -add 5 -bheight 7 -boffset 8 -mag 1 -lr -15 "$tmpbg" "$tmpbg"
-i3lock --ignore-empty-password -i "$tmpbg"
-rm "$tmpbg"
+# Takes a picture of desktop with scrot, applies glitchy effects
+# locks screen with that processed image
+# corrupter: https://github.com/r00tman/corrupter
+
+# Place to temporarily store the lockscreen image
+TMP_BG="/tmp/screen.png"
+
+# Check dependencies
+DEPENDENCIES=(
+      scrot
+      i3lock
+      corrupter
+      )
+for i in "${DEPENDENCIES[@]}"; do
+   which $i &> /dev/null || printf "$i was not found on your system.\n"
+done
+
+# Take screenshot and store it in temporary directory
+scrot --silent "$TMP_BG" # --silent removes the system BEEP
+
+# Processes and overwrites the clean image with following effects:
+ARGS=(-add 5        # 0-255 brightness control
+      -bheight 3    # average distroted block height
+      -boffset 8    # distorted block offset strength
+      -lag 0.005    # per-channel scanline lag strength
+      -lb 3         # blue scanline lag
+      -lg 3         # green scanline lag
+      -lr -15       # redline lag
+      -mag 1        # dissolve blur strength
+      -meanabber 10 # mean chromatic abberation offset
+      -stdabber 10  # std. dev. of chromatic abberation offset
+      -stdoffset 10 # std. dev. of red-blue channel offset
+      -stride 0.1   # distorted block stride strength
+      "$TMP_BG" "$TMP_BG"   # source/destination file
+     )
+corrupter "${ARGS[@]}"
+
+# Locks the screen with the processed image
+i3lock --ignore-empty-password -i "$TMP_BG"
+rm "$TMP_BG"    # Temp image cleanup
