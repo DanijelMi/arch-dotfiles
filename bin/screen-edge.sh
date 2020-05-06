8086a5d2170a34f646658a25db3bf6d17051a46a
DanijelMi <danijel.milosevic.it@gmail.com>
2020-05-06 21:19:31 +0200

    ui additions

---
+++ bin/screen-edge.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Find ideas to implement
+# Tools: xdotool + wmctrl 
+#xdotool search --name "Code" behave %@ focus exec konsole
+#xdotool search --name "Code" behave_screen_edge --delay 200 --quiesce 300 right exec alacritty
