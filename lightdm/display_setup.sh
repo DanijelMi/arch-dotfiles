c0ef5fad96e0e5b6157fb1f6f93304dc9c5661f2
DanijelMi <danijel.milosevic.it@gmail.com>
2020-05-06 21:02:39 +0200

    remove bloat

--- lightdm/display_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# If "VGA1" display exists, expand the display on the right
-xrandr | grep VGA1 > /dev/null && xrandr --output VGA1 --preferred --pos 1367x0
-
-# Not sure, recommended by the wiki wizards
-xrandr --setprovideroutputsource modesetting NVIDIA-0
-xrandr --auto
--- sxiv/README.md
+++
@@ -1,1 +0,0 @@
-This config was taken from https://github.com/LukeSmithxyz/voidrice
